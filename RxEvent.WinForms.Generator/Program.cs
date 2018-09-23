ae67ae5ebdd5029861e389af88de99dbb7a41b04
Yoshinori Ozaki <yoozaki@gmail.com>
2018-09-23 13:26:18 +0900

    First change.

---
+++ RxEvent.WinForms.Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace RxEvent.WinForms.Generator
+{
+    class Program
+    {
+        static void Main()
+        {
+            File.WriteAllText(
+                    @"..\..\..\RxEvent.WinForms\RxEvent.cs", 
+                    new EventAsObservable().TransformText());
+        }
+    }
+}
